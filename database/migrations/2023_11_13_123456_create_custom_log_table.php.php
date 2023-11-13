0e29e525778a5559b70885bdb8fd8c4290aad1b6
Mehran-tr <mehran.taheri.t@gmail.com>
2023-11-13 13:04:39 +0330

    update migration

--- database/migrations/2023_11_13_123456_create_custom_log_table.php.php
+++ database/migrations/2023_11_13_123456_create_custom_log_table.php.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomLogTable extends Migration
+class CreateLaravelEasyLogCustomLogTable extends Migration
 {
     public function up()
     {
